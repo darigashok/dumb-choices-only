5fd399a2ae07a685e33188b1dd913c2703e53f0c
Ajla Sacic <123623205+aylasacic@users.noreply.github.com>
2023-05-01 20:57:17 +0400

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ vd_studio.addEventListener('click', function () {
     menu1.style.display = "none";
     video_player2.style.display = 'block';
     video_player2.play();
-
 });
 
 im_lab.addEventListener('click', function () {
@@ -67,7 +66,6 @@ im_lab.addEventListener('click', function () {
     menu1.style.display = "none";
     video_player3.style.display = 'block';
     video_player3.play();
-
 });
 
 lobby.addEventListener('click', function () {
@@ -75,7 +73,6 @@ lobby.addEventListener('click', function () {
     menu1.style.display = "none";
     video_player4.style.display = 'block';
     video_player4.play();
-
 });
 
 vd_studio_sleep.addEventListener('click', function () {
@@ -83,7 +80,6 @@ vd_studio_sleep.addEventListener('click', function () {
     menu2.style.display = "none";
     video_player5.style.display = 'block';
     video_player5.play();
-
 });
 
 vd_studio_work.addEventListener('click', function () {
@@ -91,7 +87,6 @@ vd_studio_work.addEventListener('click', function () {
     menu2.style.display = "none";
     video_player6.style.display = 'block';
     video_player6.play();
-
 });
 
 
@@ -100,7 +95,6 @@ im_lab_inside.addEventListener('click', function () {
     menu3.style.display = "none";
     video_player7.style.display = 'block';
     video_player7.play();
-
 });
 
 im_lab_outside.addEventListener('click', function () {
@@ -108,7 +102,6 @@ im_lab_outside.addEventListener('click', function () {
     menu3.style.display = "none";
     video_player8.style.display = 'block';
     video_player8.play();
-
 });
 
 lobby_punch.addEventListener('click', function () {
@@ -116,7 +109,6 @@ lobby_punch.addEventListener('click', function () {
     menu4.style.display = "none";
     video_player9.style.display = 'block';
     video_player9.play();
-
 });
 
 lobby_getpunched.addEventListener('click', function () {
@@ -126,11 +118,8 @@ lobby_getpunched.addEventListener('click', function () {
     video_player10.play();
 });
 
-
-
 const replayButton = document.getElementById("replay-button");
 
 replayButton.addEventListener("click", function() {
   location.reload();
 });
-
